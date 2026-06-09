543e5b5a73af9e3028d13aa5f6cefaf5af927a98
uni-kakurenbo <kohma1116@gmail.com>
2026-06-09 21:26:12 +0900

    wip

--- app/server/prisma.config.ts
+++ app/server/prisma.config.ts
@@ -5,8 +5,6 @@ const DATABASE_URL =
     process.env.DATABASE_URL ||
     `mysql://${env("NS_MARIADB_USER")}:${env("NS_MARIADB_PASSWORD")}@${env("NS_MARIADB_HOSTNAME")}:${env("NS_MARIADB_PORT")}/${env("NS_MARIADB_DATABASE")}`;
 
-console.log(DATABASE_URL);
-
 export default defineConfig({
     schema: "prisma/schema.prisma",
     datasource: {
