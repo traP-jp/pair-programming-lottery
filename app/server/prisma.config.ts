c1409160680c1f274ee6af834d9f90acde810a5e
uni-kakurenbo <kohma1116@gmail.com>
2026-06-09 16:30:41 +0900

    wip

--- app/server/prisma.config.ts
+++ app/server/prisma.config.ts
@@ -1,11 +1,15 @@
 import "dotenv-expand/config";
 import { defineConfig, env } from "prisma/config";
 
+const DATABASE_URL =
+    process.env.DATABASE_URL ||
+    `mysql://${env("NS_MARIADB_USER")}:${env("NS_MARIADB_PASSWORD")}@${env("NS_MARIADB_HOSTNAME")}:${env("NS_MARIADB_PORT")}/${env("NS_MARIADB_DATABASE")}`;
+
+console.log(DATABASE_URL);
+
 export default defineConfig({
     schema: "prisma/schema.prisma",
     datasource: {
-        url:
-            env("DATABASE_URL") ||
-            `mysql://${env("NS_MARIADB_USER")}:${env("NS_MARIADB_PASSWORD")}@${env("NS_MARIADB_HOSTNAME")}:${env("NS_MARIADB_PORT")}/${env("NS_MARIADB_DATABASE")}`,
+        url: DATABASE_URL,
     },
 });
